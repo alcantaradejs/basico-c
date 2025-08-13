ff4623f4487f6ae681060a034bf24eb97899fe41
Isaque Alcantara <alcantara.isaque167@gmail.com>
2025-08-12 22:59:09 -0300

    feat: variaveis

---
+++ variaveis/vars.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int idade;     // Declara uma variável inteira chamada "idade"
+    float altura;   // Declara uma variável de ponto flutuante chamada "altura"
+    double salario; // Declara uma variável de ponto flutuante de dupla precisão chamada "salario"
+    char opcao;    // Declara uma variável de caractere chamada "opcao"
+    char string[20]; // Declara uma String ( Cadeia de caracteres ) 
+
+    return 0;
+}
