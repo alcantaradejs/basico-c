62432370ddbdfc7b7cafa87c13c9b4a98d70c5d1
Isaque Alcantara <alcantara.isaque167@gmail.com>
2025-08-14 17:23:16 -0300

    manupulando o tamanho dos tipos

---
+++ Mestre/OperadoresRelacionais/OpRelacionaisConversaoTipo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    float b = 5.0;
+
+    printf("a >= b: %d\n", a >= b);
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n", a != b); 
+
+    return 0;
+}
